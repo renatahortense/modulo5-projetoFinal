3d609014b2908c0a52aa3a35c3f3e90263b6268e
andreeigenheer <eigenheer81@gmail.com>
2021-08-28 18:02:36 -0300

    criação de componetes Alunos e Professo

--- projeto-m5/src/App.js
+++ projeto-m5/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import FormAluno from './components/Forms/FormAluno'
+import FormProf from './components/Forms/FormProf'
 
 function App() {
-  return (
-    <h1></h1>
-  );
+  return ();
 }
 
 export default App;
---
+++ projeto-m5/src/components/Forms/FormAluno.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Forms.module.css'
+
+function FormAluno(){
+    return(
+<h1>
+    André
+</h1>
+    )
+}
+
+export default FormAluno
